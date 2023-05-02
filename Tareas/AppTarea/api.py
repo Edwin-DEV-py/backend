8492eb4ea07b5698410f505aef68e9815b991ede
edwin-papasito <nicolas.tarazona.morales@gmail.com>
2023-05-02 01:41:59 -0500

    que sirva

---
+++ Tareas/AppTarea/api.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets,permissions
+from .serializer import *
+from .models import *
+
+class CrudApi(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+    permission_classes = [permissions.AllowAny]
+    
